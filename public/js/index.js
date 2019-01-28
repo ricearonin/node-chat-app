41c2e11905d12599417955d998a9e6e8c7615e83
ricearonin <taylor.a.rice@gmail.com>
2019-01-28 14:52:43 -0800

    added message template

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,19 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime,
+  });
 
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  jQuery('#messages').append(html);
 
-  jQuery('#messages').append(li);
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+  // jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function(e) {
