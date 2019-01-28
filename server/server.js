617bac66895da6a6fccb7c6eb05d25dff4dac2af
ricearonin <taylor.a.rice@gmail.com>
2019-01-28 12:14:19 -0800

    added admin greeting message and alert

--- server/server.js
+++ server/server.js
@@ -15,12 +15,32 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('new user connected');
 
+  //socket.emit from Admin text welcome to the chat app
+  //socket.broadcast.emit from Admin text New user joined
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app!',
+    createdAt: new Date().getTime(),
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User Joined!',
+    createdAt: new Date().getTime(),
+  });
+
   socket.on('createMessage', message => {
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime(),
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime,
+    // });
   });
 
   socket.on('disconnect', () => {
