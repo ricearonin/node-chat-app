1b8297e4a13af10421929e5d3b82c83c96ac5b70
ricearonin <taylor.a.rice@gmail.com>
2019-01-28 11:59:17 -0800

    Emit newMessage on createMessage:

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,7 @@
 var socket = io();
 
-const message = { body: 'hi', user: 'john' };
-
 socket.on('connect', function() {
   console.log('connected to server');
-
-  socket.emit('createMessage', message);
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,12 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'taylor',
-    text: 'see you then!',
-    createdAt: 123123,
-  });
-
   socket.on('createMessage', message => {
-    console.log(message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime(),
+    });
   });
 
   socket.on('disconnect', () => {
